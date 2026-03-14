4bb2b742f179d696bd50e294b1ba623318e937e1
Vilius Nedzveckas <viliusnedz@gmil.com>
2026-03-14 12:29:42 +0200

    First test case for buy Crypto page

--- tests/e2e/buyCrypto.spec.ts
+++ tests/e2e/buyCrypto.spec.ts
@@ -1,29 +1,50 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 import { acceptCookies } from "../../helpers/cookies.helper";
 import { BasePage } from "../../pages/BasePage";
-test.describe("search tests", () => {
+import { BuyCryptoPage } from "../../pages/BuyCryptoPage";
+test.describe("buy crypto tests", () => {
   let basePage: BasePage;
+  let buyCryptoPage: BuyCryptoPage;
+  const btcAddress = process.env.DUMMY_BTC_ADDRESS || "";
 
   test.beforeEach(async ({ page }) => {
     await page.goto("/");
     await acceptCookies(page);
     basePage = new BasePage(page);
+    buyCryptoPage = new BuyCryptoPage(page);
   });
+  //Positive TC's
+  test("Navigation to Checkout page with legacy Crypto key", async ({ page }) => {
+    const amount = "200";
+    const crypto = "BTC";
 
-  test("TODO", async ({ page }) => {
-    //TODO add some tests here
-    page.on("console", (msg) => {
-      if (msg.type() === "error") {
-        console.error(`Console error: ${msg.text()}`);
-      }
+    await basePage.clickBuyCrypto();
+    await buyCryptoPage.selectCrypto(crypto);
+    await buyCryptoPage.enterMoneyAmount(amount);
+    await buyCryptoPage.enterCryptoAddress(btcAddress);
+    await buyCryptoPage.clickContinue();
+
+    await expect(page).toHaveURL(/.*checkout\.simplexcc\.com\/next\/#\/offers/);
+    await expect(page.getByText("Credit/Debit card")).toBeVisible();
+    await expect(page.getByText("Euro Bank Transfer")).toBeVisible();
+    await expect(page.getByText("Google Pay")).toBeVisible();
+    await expect(page.getByText("Credit or Debit Card")).toBeVisible();
+  });
+});
+
+//Negative TC's
+test("TODO", async ({ page }) => {
+  // Implement negative test cases such as:
+  // - Entering an amount below the minimum limit
+  // - Entering an amount above the maximum limit
+  // - Entering non-numeric characters in the amount field
+  // - Selecting an unsupported cryptocurrency
+});
+
+test.afterEach(async ({ page }, testInfo) => {
+  if (testInfo.status !== testInfo.expectedStatus) {
+    await page.screenshot({
+      path: `screenshots/${testInfo.title}.png`,
     });
-  });
-
-  test.afterEach(async ({ page }, testInfo) => {
-    if (testInfo.status !== testInfo.expectedStatus) {
-      await page.screenshot({
-        path: `screenshots/${testInfo.title}.png`,
-      });
-    }
-  });
+  }
 });
